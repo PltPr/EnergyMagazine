4b2adcf178f80b21750c797b3c2e70bbb17fa64a
PltPr <kamil.gawlinski3@gmail.com>
2025-05-14 20:02:31 +0200

    front init

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e628cfc5ad1907dba36daedfbc813800d57998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cfc523392cf1e675c67a537539ce9e71e94d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
